4c901d6660a08bf7461e280409dfded0f249ed5e
JayTeeSF <jaytee@jayteesf.com>
2024-03-09 13:14:10 -0800

    code cleanup; add times to README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+↪ time ./letter_box.rb ube tms pai ljn
+.............
+jumpsuit, tunabilities
+jumpsuit, tenabilities
+jumpsuit, tableman
+jumpsuit, tunable
+jumpsuit, tuneable
+jumpsuit, tenable
+jumpsuit, tenantable
+jumpsuit, tentable
+
+real	0m1.756s
+user	0m1.637s
+sys	0m0.067s
+
 ./letter_box.rb ube tms pai ljn
-# example of winning words: jumpsuit, tenable
+./letter_box.rb wbs klz ady ecr # => backwards, sleazy # 29.6s thu Mar 7
+./letter_box.rb eif onu clm baq # => flanque, embolic 20.121 tue Mar 5
--- letter_box.rb
+++ letter_box.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
-WORDS_TO_SHOW = 300
-PAIRS_TO_SHOW = 25
+MAX_WORDS = 500
+MAX_RESULTS = 25
+
+# Filters a word-dictionary based on NYT letterboxed game
 class DictionaryFilter
   attr_reader :dict_path
-  attr_reader :forbidden_sequences
 
   def initialize(dict_path, letter_sets)
     @dict_path = dict_path
@@ -28,35 +29,46 @@ class DictionaryFilter
   end
 
   def filter_words
-    File.readlines(@dict_path).map(&:strip).select do |word|
-      word.chars.all? { |char| @letters.include?(char) } &&
+    filtered_word_list = []
+    File.readlines(@dict_path).map(&:strip).each do |word|
+      break if filtered_word_list.size >= MAX_WORDS
+
+      filtered_word_list.<<(word) if word.chars.all? { |char| @letters.include?(char) } &&
         @forbidden_sequences.none? { |seq| word.include?(seq) }
-    end.sort_by { |word| [-word.chars.uniq.length, word] }
+    end
+    filtered_word_list.sort_by { |word| [-word.chars.uniq.length, word] }
   end
 
   def filter_words_that_start_with(input_word)
+    filtered_word_list = []
     last_letter = input_word[-1]
     remaining_letters = @letters - input_word.chars[0..-2]
-    # warn("\nremaining letters:\n\t#{remaining_letters}\n")
 
-    File.readlines(@dict_path).map(&:strip).select do |word|
-      word.start_with?(last_letter) &&
+    File.readlines(@dict_path).map(&:strip).each do |word|
+      break if filtered_word_list.size >= MAX_WORDS
+
+      filtered_word_list.<<(word) if word.start_with?(last_letter) &&
         word.chars.all? { |char| @letters.include?(char) } &&
         remaining_letters.all? { |char| word.include?(char) } &&
         @forbidden_sequences.none? { |seq| word.include?(seq) }
-    end.sort_by { |word| [-word.chars.uniq.length, word] }
+    end
+    filtered_word_list.sort_by { |word| [-word.chars.uniq.length, word] }
   end
 
   def automated_filtering(filtered_words)
-    output_pairs = []
+    output_pairs = {}
     filtered_words.each do |word|
       $stderr.print('.')
-      break if word.size == 1
+      next if word.size == 1
+
       matches = filter_words_that_start_with(word)
-      if matches.any?
-        matches.first(WORDS_TO_SHOW).each { |match| output_pairs << [word, match] }
-        break output_pairs.size > PAIRS_TO_SHOW
+      next if matches.none?
+
+      matches.each do |match|
+        output_pairs[word] ||= []
+        output_pairs[word] << match
       end
+      break if output_pairs.keys.size > MAX_RESULTS
     end
     warn "\n"
     output_pairs
@@ -72,12 +84,11 @@ end
 dict_path = '/Users/jthomas/dev/jayteesf/config/words.txt'
 letter_sets = ARGV
 filter = DictionaryFilter.new(dict_path, letter_sets)
-#warn "\nForbidden sequences:\n\t#{filter.forbidden_sequences}\n\n"
 filtered_words = filter.filter_words
 reversed_filtered_words = []
-filtered_words.first(WORDS_TO_SHOW).reverse.each { |word| reversed_filtered_words << word }
+filtered_words.reverse.each { |word| reversed_filtered_words << word }
 
-# puts "Top #{WORDS_TO_SHOW} filtered words:"
+# puts "Top #{MAX_WORDS} filtered words:"
 # reversed_filtered_words.each { |word| puts word }
 
-filter.automated_filtering(reversed_filtered_words).each { |pair| puts pair.join(', ') }
+filter.automated_filtering(reversed_filtered_words).each_pair { |(k, v)| puts "#{k} -> #{v}" }
