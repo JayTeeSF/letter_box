06b53e6e42fdde5ce1d353101d39956fabba6371
JayTeeSF <jaytee@jayteesf.com>
2024-03-09 11:41:59 -0800

    Fix filtering

--- letter_box.rb
+++ letter_box.rb
@@ -2,6 +2,7 @@
 
 class DictionaryFilter
   attr_reader :dict_path
+  attr_reader :forbidden_sequences
 
   def initialize(dict_path)
     @dict_path = dict_path
@@ -20,6 +21,7 @@ class DictionaryFilter
   end
 
   def generate_forbidden_sequences(set)
+    set.chars.each { |char| @forbidden_sequences << char * 2 }
     set.chars.permutation(2).each { |seq| @forbidden_sequences << seq.join }
     @forbidden_sequences.uniq!
   end
@@ -27,19 +29,20 @@ class DictionaryFilter
   def filter_words
     File.readlines(@dict_path).map(&:strip).select do |word|
       word.chars.all? { |char| @letters.include?(char) } &&
-        !@forbidden_sequences.any? { |seq| word.include?(seq) }
-    end.sort_by(&:length).reverse
+        @forbidden_sequences.none? { |seq| word.include?(seq) }
+    end.sort_by { |word| [-word.chars.uniq.length, word] }
   end
 
   def filter_words_that_start_with(input_word)
     last_letter = input_word[-1]
-    remaining_letters = @letters - input_word.chars
+    remaining_letters = @letters - input_word.chars[1..-1]
 
     File.readlines(@dict_path).map(&:strip).select do |word|
       word.start_with?(last_letter) &&
+        word.chars.all? { |char| @letters.include?(char) } &&
         remaining_letters.all? { |char| word.include?(char) } &&
-        !@forbidden_sequences.any? { |seq| word.include?(seq) }
-    end.sort_by(&:length).reverse
+        @forbidden_sequences.none? { |seq| word.include?(seq) }
+    end.sort_by { |word| [-word.chars.uniq.length, word] }
   end
 
   def prompt_for_words_and_filter(filtered_words)
@@ -62,6 +65,7 @@ end
 dict_path = '/Users/jthomas/dev/jayteesf/config/words.txt'
 filter = DictionaryFilter.new(dict_path)
 filter.prompt_for_letter_sets
+warn "\nForbidden sequences:\n\t#{filter.forbidden_sequences}\n\n"
 filtered_words = filter.filter_words
 
 puts 'Top 100 filtered words:'
